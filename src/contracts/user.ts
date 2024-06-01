9ab7329c216139ad21150fc7fae6aee15698715f
Omar A. H. Abo El Wafa <58788836+omarAboElWafa@users.noreply.github.com>
2024-06-01 14:08:27 +0300

    feat: Add registered_date to the user interface

--- src/contracts/user.ts
+++ src/contracts/user.ts
@@ -1,19 +1,19 @@
 export interface IUser {
     id: string
-    firstName: string
-    lastName: string
+    name: string
     email: string
     password: string,
     phone?: string,
     type?: string
+    registered_date: string
 }
 
 
 export interface IUserUpdate {
-    firstName?: string
-    lastName?: string
+    name?: string
     email?: string
     password?: string,
     phone?: string,
     type?: string
+    registered_date?: string
 }
