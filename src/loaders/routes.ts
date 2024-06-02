21d2ff87ff7bcbf2ef4db462f188ad86bb95d0ef
Omar A. H. Abo El Wafa <58788836+omarAboElWafa@users.noreply.github.com>
2024-06-02 03:17:19 +0300

    fix: Register loans router to the main app

--- src/loaders/routes.ts
+++ src/loaders/routes.ts
@@ -1,8 +1,10 @@
 import { Express } from 'express';
 import userModule from '../components/identity/identity.module';
 import bookModule from '../components/book/book.module';
+import loanModule from '../components/loan/loan.module';
 
 export default (app : Express) => {
     app.use('/auth', userModule.router);
     app.use('/books', bookModule.router);
+    app.use('/loans', loanModule.router);
 }
